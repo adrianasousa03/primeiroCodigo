6f518e10d5825622d968de889c7e8a7f375c0e6b
Adriana Sousa <>
2021-04-17 03:05:07 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+var nome = "Adriana" 
+alert(nome)
+
+var idade = 48
+alert (idade)
+
+var idadeUsuario = prompt("Quantos anos você tem?")
+var idadeUsuarioComoNumero = parseInt(idadeUsuario)
+
+idadeUsuarioComoNumero = idadeUsuarioComoNumero + 1
+alert (idadeUsuarioComoNumero)
